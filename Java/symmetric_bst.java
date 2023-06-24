0cf09c93ab2dced3ff2a5870dab390f9f9da814b
Yash Pandey <109035687+yashpandey474@users.noreply.github.com>
2023-06-24 16:24:58 +0530

    Create symmetric_bst.java

---
+++ Java/symmetric_bst.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public static boolean recSymmetric(TreeNode root1, TreeNode root2){
+        if(root1 == null && root2 == null){
+            return true;
+        }
+        if(root1 != null && root2 != null && (root1.val == root2.val)){
+            return recSymmetric(root1.right, root2.left) && recSymmetric(root1.left, root2.right);
+        }
+        
+        return false;
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        return recSymmetric(root.left, root.right);
+    }
+}
