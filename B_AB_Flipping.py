4a7b9c668559c04fca93dd17b316409a05f27bd2
Yash Pandey <ypandey474@gmail.com>
2024-01-05 23:45:53 +0530

    Codeforces problems

---
+++ B_AB_Flipping.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    s = list(str(input()))
+    
+    # S CONSISTS OF CHARACTERS A AND B
+    # I; Si = A and Si+1 = B, swap SI AND SI+1
+    # AT MOST ONCE FOR EACH INDEX
+    #MAX NUMBER OF OPERATIONS
+    
+    
