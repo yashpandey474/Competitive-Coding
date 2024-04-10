ad7c1e5e1618008f75499b963e72a05edd915e57
Yash Pandey <ypandey474@gmail.com>
2024-04-10 23:41:16 +0530

    More questions

---
+++ B_Ugu.py
@@ -0,0 +1,7 @@
+t = int(input())
+
+for test in range(t):
+    n = int(input())
+    s = input()
+
+    
---
+++ B_XOR_Specia_LIS_t.py
@@ -0,0 +1,7 @@
+t = int(input())
+
+for test in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    
